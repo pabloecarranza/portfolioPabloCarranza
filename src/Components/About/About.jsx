a3d58917285d4afe2f74016ce8ae2cf7af3f2099
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2022-09-20 02:04:06 -0300

    upgradePortfolio

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,46 +1,67 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
     <div id="about">
-        <div className="title">
-            <h2>Sobre Mi</h2>
-        </div>
+      <div className="title">
+        <h2>Sobre Mi</h2>
+      </div>
 
-        <div className="container about-container">
-            <div className="about-content">
-                 <div className="about-cards">
-                     <div className="about-bg-card">
-                     <article className="about-card">
-                        <i class="fa-solid fa-briefcase"></i>
-                         <h4>Experiencia</h4>
-                         <small>2 años</small>
-                     </article>
-                     </div>
-                     <div className="about-bg-card">
-                     <article className="about-card">
-                        <i class="fa-solid fa-code"></i>
-                         <h4>Conocimientos</h4>
-                         <small>JavaScript, React, Redux, NodeJS, Express, postgreSQL</small>
-                     </article>
-                     </div>
-                     <div className="about-bg-card">
-                     <article className="about-card">
-                        <i class="fa-solid fa-palette"></i>
-                         <h4>Maquetación & Diseño</h4>
-                         <small>CSS, SASS, LESS, PostCSS, TailwindCSS, BootStrap, FIgma</small>
-                     </article>
-                     </div>
-                 </div>
-                    <div className="about-text">
-                 <p>Hola! soy Pablo Carranza soy Desarrollador FullStack. Inicie en el mundo de la programación hace dos años de manera autodidacta, nunca me imaginé la pasión que me iba a generar. Por este motivo hace un año decidí empezar a certificarme para demostrar mis conocimientos y capacidades mediante un Bootcamp y fue una experiencia unica de muchisimo aprendizaje.
-                     Me considero una persona dinámica, responsable y un gran jugador en equipo, con muchas ganas de seguir aprendiendo nuevas tecnologías.</p>
-                     </div>
+      <div className="container about-container">
+        <div className="about-content">
+          <div className="about-cards">
+            <div className="about-bg-card">
+              <article className="about-card">
+                <i class="fa-solid fa-briefcase"></i>
+                <h4>Experiencia</h4>
+                <small>2 años</small>
+              </article>
             </div>
+            <div className="about-bg-card">
+              <article className="about-card">
+                <i class="fa-solid fa-code"></i>
+                <h4>Conocimientos</h4>
+                <small>
+                  JavaScript, React, Redux, NodeJS, Express, postgreSQL
+                </small>
+              </article>
+            </div>
+            <div className="about-bg-card">
+              <article className="about-card">
+                <i class="fa-solid fa-palette"></i>
+                <h4>Maquetación & Diseño</h4>
+                <small>
+                  CSS, SASS, LESS, PostCSS, TailwindCSS, BootStrap, FIgma
+                </small>
+              </article>
+            </div>
+          </div>
+          <div className="about-text">
+            <p>
+              ¡Hola!, Soy Desarrollador Fullstack con orientación a Frontend
+              apasionado por el aprendizaje. Me eh formado mediante
+              instituciones educativas y de manera constante como autodidacta.{" "}
+              <br />
+              <br />
+              Mis tecnologías son HTML5, CSS, Boostrap, ChakraUI, MaterialUI,
+              JavaScript, React, Redux, NodeJS, Express, Sequelize, MongoDB,
+              PostgreSQL, MySQL y actualmente me encuentro aprendiendo
+              Typescript, React Native y NextJS. <br /> <br />
+              He trabajado en entornos profesionales con metodologías ágiles
+              SCRUM utilizando Trello/Jira y GitFlow/GitLab. Soy muy
+              comunicativo, me adapto rápidamente al cambio y me gusta mucho el
+              trabajo en equipo en los que se promueva la excelencia y el
+              mejoramiento continuo. <br /> <br />
+              Quieres conocerme mejor? Te dejo mi contacto: <br /> Mail:
+              📬pabloecarranza@gmail.com <br /> Github: 🐱
+              https://github.com/pabloecarranza
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,82 +1,154 @@
-import React from 'react';
-import './Projects.css';
-import Pelicano from '../../img/Pelicano.jpg';
+import React from "react";
+import "./Projects.css";
+import Pelicano from "../../img/Pelicano.jpg";
+import Taskeep from "../../img/TASKEEP.jpg";
+import OpenWeatherApp from "../../img/OpenWeatherApp-.jpg";
 
-import OpenWeatherApp from '../../img/OpenWeatherApp-.jpg';
-
-import Podedex from '../../img/Pokedex.jpg';
+import Podedex from "../../img/Pokedex.jpg";
 
 const Projects = () => {
   return (
-   <div id="projects">
-       <div className="title">
-           <h3>Proyectos</h3>
-       </div>
+    <div id="projects">
+      <div className="title">
+        <h3>Proyectos</h3>
+      </div>
 
-       <div className="cards">
-            <div className="card">
-                <div className="img-card">
-                <img src={Pelicano } alt="Pelicano" />
-                </div>
-                <div className="content-card">
-                    <div className="details">
-                        <h2>Pelicano PetStore</h2>
-                        <h3>Aplicación Web</h3>
-                        <br />
-                        <div className="data">
-                        <p>Pagina Web responsiva para para emprendimiento de articulos para mascostas
-                        </p>
-                        </div>
-                        <div className="btn-action">
-                            <a href='https://github.com/pabloecarranza/pelicano_petstore' target='_blank' rel="noreferrer">GitHub</a>
-                            <a href='https://pabloecarranza.github.io/pelicano_petstore/pages/home.html' target='_blank' rel="noreferrer">Deploy</a>
-                        </div>
-                    </div>
-                </div>
+      <div className="cards">
+        <div className="card">
+          <div className="img-card">
+            <img src={Pelicano} alt="Pelicano" />
+          </div>
+          <div className="content-card">
+            <div className="details">
+              <h2>Pelicano PetStore</h2>
+              <h3>Aplicación Web</h3>
+              <br />
+              <div className="data">
+                <p>
+                  Pagina Web responsiva para para emprendimiento de articulos
+                  para mascostas
+                </p>
+              </div>
+              <div className="btn-action">
+                <a
+                  href="https://github.com/pabloecarranza/pelicano_petstore"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+                <a
+                  href="https://pabloecarranza.github.io/pelicano_petstore/pages/home.html"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Deploy
+                </a>
+              </div>
             </div>
-            <div className="card">
-                <div className="img-card">
-                <img src={OpenWeatherApp} alt="Open Weather App" />
-                </div>
-                <div className="content-card">
-                    <div className="details">
-                        <h2>Open Weather App</h2>
-                        <h3>Aplicación de Clima</h3>
-                        <div className="data">
-                        <p>Aplicación Web para consultar el clima mediante buscador y con geolocalizacion con 5 ciudades preseteadas.
-                        </p>
-                        </div>
-                        <div className="btn-action">
-                            <a href='https://github.com/pabloecarranza/FrontEnd-React---Flow' target='_blank' rel="noreferrer">GitHub</a>
-                            <a href='https://front-end-react-flow-j0kxhkkfw-pabloecarranza.vercel.app/' target='_blank' rel="noreferrer">Deploy</a>
-                        </div>
-                    </div>
-                </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="img-card">
+            <img src={OpenWeatherApp} alt="Open Weather App" />
+          </div>
+          <div className="content-card">
+            <div className="details">
+              <h2>Open Weather App</h2>
+              <h3>Aplicación de Clima</h3>
+              <div className="data">
+                <p>
+                  Aplicación Web para consultar el clima mediante buscador y con
+                  geolocalizacion con 5 ciudades preseteadas.
+                </p>
+              </div>
+              <div className="btn-action">
+                <a
+                  href="https://github.com/pabloecarranza/FrontEnd-React---Flow"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+                <a
+                  href="https://front-end-react-flow-j0kxhkkfw-pabloecarranza.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Deploy
+                </a>
+              </div>
             </div>
-            <div className="card">
-                <div className="img-card">
-                    <img src={Podedex} alt="Pokedex" />
-                </div>
-                <div className="content-card">
-                    <div className="details">
-                        <h2>Pokedex</h2>
-                        <h3>Aplicación Pokemon API</h3>
-                        <br />
-                        <br />
-                        <div className="data">
-                        <p>Aplicacion de consumo de API de Pokemon
-                        </p>
-                        </div>
-                        <div className="btn-action">
-                            <a href='https://github.com/pabloecarranza/NewPokedex' target='_blank' rel="noreferrer">GitHub</a>
-                            <a href='https://new-pokedex.vercel.app/' target='_blank' rel="noreferrer">Deploy</a>
-                        </div>
-                    </div>
-                </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="img-card">
+            <img src={Taskeep} alt="Pokedex" />
+          </div>
+          <div className="content-card">
+            <div className="details">
+              <h2>TASKEEP</h2>
+              <h3>Aplicación de Tareas</h3>
+              <br />
+              <br />
+              <br />
+              <div className="data">
+                <p>Proyecto FullStack PERN</p>
+              </div>
+              <div className="btn-action">
+                <a
+                  href="https://github.com/pabloecarranza/NewPokedex"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+                <a
+                  href="https://github.com/pabloecarranza/taskeep"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Deploy
+                </a>
+              </div>
             </div>
-       </div>
-   </div>
-  )
-}
+          </div>
+        </div>
+        <div className="card">
+          <div className="img-card">
+            <img src={Podedex} alt="Pokedex" />
+          </div>
+          <div className="content-card">
+            <div className="details">
+              <h2>Pokedex</h2>
+              <h3>Aplicación Pokemon API</h3>
+              <br />
+              <br />
+              <div className="data">
+                <p>Aplicacion de consumo de API de Pokemon</p>
+              </div>
+              <div className="btn-action">
+                <a
+                  href="https://github.com/pabloecarranza/NewPokedex"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+                <a
+                  href="https://new-pokedex.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Deploy
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
